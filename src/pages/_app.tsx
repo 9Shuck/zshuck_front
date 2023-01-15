9e01d67dc2c54430153bac16928e3cb55ad2adad
Rubén Pérez Herrando <shuck@Rubens-MacBook-Air.local>
2023-01-15 22:36:48 +0100

    responsive of subfooter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import NavBar from '@/components/navbar'
+import Footer from '@/components/subfooter'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="app">
       <NavBar/>
       <Component {...pageProps} />
-    </>
+      <Footer/>
+    </div>
   )
 }
