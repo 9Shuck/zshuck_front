7b0813aecd7388945e53279b9dccb5c8049f524e
Rubén Pérez Herrando <shuck@Rubens-MacBook-Air.local>
2023-01-15 20:59:28 +0100

    home draft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,25 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="homeContainer">
+      <div className="homeContainer__header">
+        <div className="homeContainer__header__pretitle">
+          DESARROLLO DE APLICACIONES WEB
+        </div>
+        <div className="homeContainer__header__title">
+          <p>
+            <span className="secondary-color">z</span>shuck
+            <span className="font-md"> by <span className="secondary-color">Rubén Pérez Herrando</span></span>
+          </p>
+        </div>
+        <div className="homeContainer__header__photo"></div>
+        <div className="homeContainer__header__description">
+          <p>Hola 👋🏽 mi nombre es Rubén Pérez y soy full-stack developer. 
+          Desarrollo todo tipo de páginas web y aplicaciones. Puedes seguirme
+          en mis redes para ver consejos sobre programación.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
