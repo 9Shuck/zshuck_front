94303715d935a2831051c93f848436d7bb730d9c
Rubén Pérez Herrando <shuck@Rubens-MacBook-Air.local>
2023-01-31 14:36:41 +0100

    hotfixing media queries and css

--- src/components/technologyWidget.tsx
+++ src/components/technologyWidget.tsx
@@ -53,8 +53,8 @@ function TechnologyWidget(){
                 <a className="technologyLink" href={`/${x.link}`}>
                     <div className="technologyCard">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" width="50" height="55"/>
-                        <div>{x.name}</div>
-                        <div>{x.stack}</div>
+                        <div className="technologyCard__text">{x.name}</div>
+                        <div className="technologyCard__stack">{x.stack}</div>
                     </div>
 
                 </a>
